23307dbaef1fd6b31b2ad6aefeb7a54b57a22974
赵乾 <zhaoqian@zhaoqiandeMacBook-Air.local>
2021-07-07 01:25:04 +0800

    内置类

--- About_python.py
+++ About_python.py
@@ -3,6 +3,7 @@ import os
 
 
 def main():
+    # 感受Python变量类型
     temp = 18.6
     print("temp的类型是：" + str(type(temp)) + "，temp的值是：" + str(temp))
     orig = temp
@@ -12,7 +13,8 @@ def main():
     print("修改temp后，orig的值是：" + str(orig))
 
 def typeOfPython():
-    typePython = input("请输入数据类型：")
+    # Python内置的类
+    typePython = input("请输入一种Python内置类的数据类型（bool、int、float、list、tuple、str、set、dict）：")
     if typePython == "bool":
         print("bool类只有两个值：True和False，构造函数bool()默认返回False；0和空也表示False，其他表示True")
     elif typePython == "int" or typePython == "float":
@@ -32,9 +34,16 @@ def typeOfPython():
         print("list实例存储对象序列，列表是一个参考结构，在技术上存储元素的引用序列。列表采用0索引，因此一个长度为n的列表包含从0到n-1的元素。构造函数list()产生一个空列表，python使用[]作为列表的分隔符")
     elif typePython == "tuple":
         print("tuple是序列的一个不可变版本，Python使用()作为元组的分隔符。为了与带括号的表达式区别开，只有一个元素的元组表示为(17,)，元素后必须有一个逗号")
+    elif typePython == "str":
+        print("str专门用来代表一种不变的字符序列，它基于Unicode国际字符集。字符串里可以使用反斜杠作为转义字符，比如'20 \\u20AC'可以表示20 \u20AC")
+    elif typePython == "set":
+        print("set表示一个集合的数学概念，即许多元素的集合，集合中没有重复元素，而且这些元素没有内在联系。--只有不可变类型的实例才可以被添加到集合里。构造函数set()表示一个空集合，集合用{}作为集合的分隔符")
+    elif typePython == "dict":
+        print("dict表示字典或者映射，即从一组不同的键中找到对应的值，构造函数可用{}")
     else:
         print("没有该数据类型")
 
 
 if __name__ == '__main__':
+    main()
     typeOfPython()
